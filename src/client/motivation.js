16d15c1cf6ff902ecde0d12a46105744ae543d98
w1ll14mc41g <162731107+w1ll14mc41g@users.noreply.github.com>
2024-04-23 16:51:43 -0400

    added motivation tips

--- src/client/motivation.js
+++ src/client/motivation.js
@@ -1,16 +1,23 @@
 // Motivation Menu
-const random_tip = document.getElementById("random_tip");
 const success_button = document.getElementById("success_button");
-const success_menu = document.getElementById("success_menu")
+const success_menu = document.getElementById("success_menu");
+const random_tip = document.getElementById("random_tip");
 
-tips = [
+
+const tips = [
     "\"It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.\" - William James",
     "\"Success is not final; failure is not fatal: It is the courage to continue that counts.\" - Winston Churchill",
     "\"Education is the most powerful weapon which you can use to change the world.\" - Nelson Mandela",
-    "\"Life isn't about finding yourself. Life is about creating yourself.\" - George Bernard Shaw"
-]
+    "\"You've got to get up every morning with determination if you're going to go to bed with satisfaction.\" —George Lorimer",
+    "\"Life isn't about finding yourself. Life is about creating yourself.\" - George Bernard Shaw",
+    "\"Don't let yesterday take up too much of today.\" - Will Rogers"
+];
 
 
 success_button.addEventListener("click", function() {
 
+});
+
+random_tip.addEventListener("click", function() {
+    const tip = tips[Math.floor(Math.random() * tips.length)];
 });

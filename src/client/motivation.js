68bd4d51840d01959bfb5f5664f3277372fc001d
w1ll14mc41g <162731107+w1ll14mc41g@users.noreply.github.com>
2024-04-23 16:43:12 -0400

    motivation.js

---
+++ src/client/motivation.js
@@ -0,0 +1,16 @@
+// Motivation Menu
+const random_tip = document.getElementById("random_tip");
+const success_button = document.getElementById("success_button");
+const success_menu = document.getElementById("success_menu")
+
+tips = [
+    "\"It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.\" - William James",
+    "\"Success is not final; failure is not fatal: It is the courage to continue that counts.\" - Winston Churchill",
+    "\"Education is the most powerful weapon which you can use to change the world.\" - Nelson Mandela",
+    "\"Life isn't about finding yourself. Life is about creating yourself.\" - George Bernard Shaw"
+]
+
+
+success_button.addEventListener("click", function() {
+
+});

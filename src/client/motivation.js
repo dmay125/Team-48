c0a6910607e3a7effbf9155b17c5e89de2ea6c83
w1ll14mc41g <162731107+w1ll14mc41g@users.noreply.github.com>
2024-04-24 12:33:45 -0400

    more motivation tips

--- src/client/motivation.js
+++ src/client/motivation.js
@@ -5,15 +5,24 @@ const success_menu = document.getElementById("success_menu");
 const students_button = document.getElementById("students_button");
 const students_menu = document.getElementById("students_menu");
 
+const personalLife_button = document.getElementById("personalLife_button");
+const personalLife_menu = document.getElementById("personalLife_menu");
+
 const random_tip = document.getElementById("random_tip");
 
 const tips = [
     "\"It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.\" - William James",
     "\"Success is not final; failure is not fatal: It is the courage to continue that counts.\" - Winston Churchill",
+    "\"It is better to fail in originality than to succeed in imitation.\" - Herman Melville",
+    "\"Success usually comes to those who are too busy to be looking for it.\" - Henry David Thoreau",
     "\"Education is the most powerful weapon which you can use to change the world.\" - Nelson Mandela",
     "\"You've got to get up every morning with determination if you're going to go to bed with satisfaction.\" —George Lorimer",
+    "\"The most difficult thing is the decision to act; the rest is merely tenacity.\" - Amelia Earhart",
+    "\"A little progress each day adds up to big results.\" - Satya Nani",
     "\"Life isn't about finding yourself. Life is about creating yourself.\" - George Bernard Shaw",
-    "\"Don't let yesterday take up too much of today.\" - Will Rogers"
+    "\"Don't let yesterday take up too much of today.\" - Will Rogers",
+    "\"If you are working on something that you really care about, you don't have to be pushed. The vision pulls you.\" - Steve Jobs",
+    "\"Goal setting is the secret to a compelling future.\" - Tony Robbins"
 ];
 
 
@@ -30,6 +39,14 @@ students_button.addEventListener("click", function() {
         students_menu.style.display = "block";
     } else {
         students_menu.style.display = "none";
+    }
+});
+
+personalLife_button.addEventListener("click", function() {
+    if (personalLife_menu.style.display === "none") {
+        personalLife_menu.style.display = "block";
+    } else {
+        personalLife_menu.style.display = "none";
     }
 });
 

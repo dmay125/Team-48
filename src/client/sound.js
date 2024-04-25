5720ac51e3b87b019ab2e79495f1d3210d3fa15b
Sandy Son <sdson@umass.edu>
2024-04-24 21:54:38 -0400

    added sound prev + next functionality

--- src/client/sound.js
+++ src/client/sound.js
@@ -44,6 +44,28 @@ function pauseSong() {
     audio.pause();
 }
 
+function prevSong() {
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
 // event listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
@@ -54,3 +76,6 @@ playBtn.addEventListener('click', () => {
         playSong();
     }
 })
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
